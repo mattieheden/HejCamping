4e619493521a60080ae47e6199330ec32f2fe55b
84patjak <patrik.jakobsson84@gmail.com>
2024-08-15 21:49:54 +0200

    CAM-16 Har lagt till funktion för bokning samt bokningsbekräftelse, ingen felhantering än.

--- Models/BookingModel.cs
+++ Models/BookingModel.cs
@@ -2,10 +2,10 @@ namespace HejCamping.Models
 {
     public class BookingModel
     {
-        public List<Campsite> Campsites { get; set; }
+        public int BookingId { get; set; }
         public DateTime FromDate { get; set; }
         public DateTime ToDate { get; set; }
-        public int SelectedCampsiteId { get; set; }
+        public int CampsiteId { get; set; }
         public string Name { get; set; }
         public string Email { get; set; }
     }
