5070fe5bd50c6888a19140bc291e544fd210740d
84patjak <patrik.jakobsson84@gmail.com>
2024-08-15 22:46:19 +0200

    Cam-16 småfix

--- Models/BookingModel.cs
+++ Models/BookingModel.cs
@@ -5,7 +5,7 @@ namespace HejCamping.Models
         public int BookingId { get; set; }
         public DateTime FromDate { get; set; }
         public DateTime ToDate { get; set; }
-        public int CampsiteId { get; set; }
+        public int CabinId { get; set; }
         public string Name { get; set; }
         public string Email { get; set; }
     }
