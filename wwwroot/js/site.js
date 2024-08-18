766b407f5a961312d9ce24a57eb4e3157add3ce2
Leonard Cogles Bonne <coglescode@gmail.com>
2024-08-18 19:20:45 +0200

    Added basic styling for redirect message modal.

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,22 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+// JavaScript code
+document.addEventListener("DOMContentLoaded", function() {
+  // Get the modal
+  var modal = document.getElementById("modal");
+
+  // Delay in milliseconds (e.g., 3000ms = 3 seconds)
+  var delay1 = 3000;
+  
+  var delay2 = 7000
+
+  // Show the modal after the delay
+  setTimeout(function() {
+      modal.style.display = "block";
+  }, delay1);
+
+  //setTimeout(function() {
+  //    modal.style.display = "none";
+  //}, delay2);
+});
