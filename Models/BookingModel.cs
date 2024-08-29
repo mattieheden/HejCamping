f574e500491df3e810445b3c39e588aa4af7d1c3
Pjakobsson <109985297+Pjakobsson@users.noreply.github.com>
2024-08-29 15:02:49 +0200

    Total price is now displayed under Book button. Price is also displayed on available cabins when hovering over it on map

--- Models/BookingModel.cs
+++ Models/BookingModel.cs
@@ -7,8 +7,8 @@ namespace HejCamping.Models
         public DateTime ToDate { get; set; }
         public DateTime OrderDate { get; set; }
         public int CabinId { get; set; }
-        public string Name { get; set; }
-        public string Email { get; set; }
+        public string? Name { get; set; }
+        public string? Email { get; set; }
         public float PricePerNight { get; set; }  
         public int NumberOfNights { get; set; }     
         public float TotalPrice { get; set; } 
